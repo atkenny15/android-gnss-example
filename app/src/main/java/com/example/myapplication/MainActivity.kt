af57fd5b55061f9fb9733a4cf2fd5b6bba8ff5ac
Andy Kenny <atkenny15@gmail.com>
2024-10-13 22:04:13 -0400

    Scanning and BluetoothGattCallabck

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
@@ -28,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     private lateinit var bluetoothAdapter: BluetoothAdapter
@@ -58,6 +60,9 @@ class MainActivity : ComponentActivity() {
                 isScanning = false
             }))
         }
+        var gatt by remember {
+            mutableStateOf<BluetoothGatt?>(null)
+        }
 
         Column(
             modifier = Modifier
@@ -71,6 +76,13 @@ class MainActivity : ComponentActivity() {
                     stopScan(scanCb)
                     isScanning = false
                 } else {
+                    if (gatt != null) {
+                        gatt!!.disconnect()
+                        gatt = null
+                    }
+                    if (scanCb.result != null) {
+                        scanCb.result = null
+                    }
                     if (checkPermissions()) {
                         Log.i("tag", "startScan()")
                         startScan(scanCb)
@@ -84,10 +96,17 @@ class MainActivity : ComponentActivity() {
             }
 
             if (!isScanning && scanCb.result != null) {
-                Button({
-                    pairDevice(scanCb.result?.device!!)
-                }) {
-                    Text("Pair")
+                Row {
+                    Button({
+                        pairDevice(scanCb.result?.device!!)
+                    }) {
+                        Text("Pair")
+                    }
+                    Button({
+                        gatt = setupGatt(scanCb.result!!.device!!, gatt)
+                    }) {
+                        Text("Test GATT")
+                    }
                 }
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -97,20 +116,33 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun setupGatt(dev: BluetoothDevice, gatt: BluetoothGatt?): BluetoothGatt? {
+        if (gatt == null) {
+            return dev.connectGatt(this, false, MyBluetoothGattCallback())
+        } else {
+            gatt.disconnect()
+            return null
+        }
+    }
+
     private fun pairDevice(dev: BluetoothDevice) {
-        dev.connectGatt(this, true, MyBluetoothGattCallback())
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("tag", "missing BLUETOOTH_CONNECT permission")
+            return
+        }
+        dev.createBond()
     }
 
     private fun startScan(scanCb: MyScanCallback) {
         if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                this, Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_SCAN
+                this, Manifest.permission.BLUETOOTH_SCAN
             ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_CONNECT
+                this, Manifest.permission.BLUETOOTH_CONNECT
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             Log.e("tag", "missing BLUETOOTH_SCAN permission")
@@ -136,33 +168,26 @@ class MainActivity : ComponentActivity() {
 
     private fun checkPermissions(): Boolean {
         return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
+            this, Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.BLUETOOTH_SCAN
-        ) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) == PackageManager.PERMISSION_GRANTED
+            this, Manifest.permission.BLUETOOTH_SCAN
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            this, Manifest.permission.BLUETOOTH_CONNECT
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun requestPermissions() {
         ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN,
+            this, arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.BLUETOOTH_SCAN,
                 Manifest.permission.BLUETOOTH_CONNECT
-            ),
-            1
+            ), 1
         )
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -173,9 +198,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class MyScanCallback(onFinish: (MyScanCallback) -> Unit)  : ScanCallback() {
+class MyScanCallback(onFinish: (MyScanCallback) -> Unit) : ScanCallback() {
     var result: ScanResult? = null
     val onFinish = onFinish
+
+    private fun isMatch(result: ScanResult): Boolean {
+        if (result.device.name == "FlySight") {
+            var s = String()
+            val mDat = result.scanRecord?.manufacturerSpecificData;
+            if (mDat == null) {
+                s += "mDat=${mDat}\n"
+            } else {
+                s += "mDat.size()=${mDat.size()}\n"
+                for (index in 0..<mDat.size()) {
+                    val value = mDat.valueAt(index)
+                    s += "${index}: key=${mDat.keyAt(index)} val=["
+                    for (byte in value) {
+                        s += "${byte},"
+                    }
+                    s += "]\n"
+                }
+            }
+
+            Log.i("tag", "found: ${s}")
+            this.result = result
+            onFinish(this)
+            return true
+        }
+
+        return false
+    }
 
     override fun onScanResult(callbackType: Int, result: ScanResult) {
         super.onScanResult(callbackType, result)
@@ -185,11 +237,7 @@ class MyScanCallback(onFinish: (MyScanCallback) -> Unit)  : ScanCallback() {
             return
         }
 
-        if (result.device.name == "FlySight") {
-            Log.i("tag", "found")
-            this.result = result
-            onFinish(this)
-        }
+        isMatch(result)
     }
 
     override fun onBatchScanResults(results: List<ScanResult>) {
@@ -201,6 +249,9 @@ class MyScanCallback(onFinish: (MyScanCallback) -> Unit)  : ScanCallback() {
 
         for (result in results) {
             Log.i("tag", "found ${result.device.name} : ${result.device.address}")
+            if (isMatch(result)) {
+                break;
+            }
             if (result.device.name == "FlySight") {
                 Log.i("tag", "found")
                 this.result = result
@@ -217,15 +268,22 @@ class MyScanCallback(onFinish: (MyScanCallback) -> Unit)  : ScanCallback() {
 }
 
 class MyBluetoothGattCallback : BluetoothGattCallback() {
+    val GNSS_PV_UUID = UUID.fromString("00000000-8e22-4541-9d4c-21edae82ed19");
+
+    //val CRS_TX_UUID = UUID.fromString("00000001-8e22-4541-9d4c-21edae82ed19")
+    val CRS_RX_UUID = UUID.fromString("00000002-8e22-4541-9d4c-21edae82ed19");
+
     override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+        super.onConnectionStateChange(gatt, status, newState)
+
         Log.i("tag", "state change $status $newState")
 
         when (newState) {
             BluetoothProfile.STATE_CONNECTED -> {
                 Log.i("tag", "connected")
-                // Successfully connected to the device
-                gatt.discoverServices() // Discover services after successful connection
+                gatt.discoverServices()
             }
+
             BluetoothProfile.STATE_DISCONNECTED -> {
                 Log.i("tag", "disconnected")
                 // Device disconnected
@@ -235,45 +293,84 @@ class MyBluetoothGattCallback : BluetoothGattCallback() {
     }
 
     override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+        super.onServicesDiscovered(gatt, status)
+
         Log.i("tag", "services discovered $status")
+
         if (status == BluetoothGatt.GATT_SUCCESS) {
             // Services discovered, you can now read/write characteristics
             val services = gatt.services
             // Example: Reading a characteristic
             services.forEach { service ->
-                /*
-                val characteristic = service.getCharacteristic(YOUR_CHARACTERISTIC_UUID)
-                if (characteristic != null) {
-                    gatt.readCharacteristic(characteristic) // Read characteristic
+                Log.i("tag", "service ${service.uuid.toString()}")
+                for (char in service.characteristics) {
+                    Log.i("tag", "char: ${char.uuid.toString()}")
                 }
-                */
+
+
+                var char = service.getCharacteristic(GNSS_PV_UUID)
+                if (char != null) {
+                    var desc = char.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"))
+                    if (desc == null) {
+                        Log.e("tag", "desc is null")
+                        /*
+                        desc = BluetoothGattDescriptor(
+                            GNSS_PV_UUID, BluetoothGattDescriptor.PERMISSION_READ_ENCRYPTED
+                        )
+                        char.addDescriptor(desc)
+                        */
+                    }
+
+                    if (gatt.setCharacteristicNotification(char, true)) {
+                        Log.i("tag", "notification enabled")
+                    } else {
+                        Log.e("tag", "notification could not be enabled")
+                    }
+                    val wr_status = gatt.writeDescriptor(
+                        desc, BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                    )
+                    Log.i("tag", "gnss write status: $wr_status")
+
+                }
             }
         }
     }
 
-    override fun onCharacteristicRead(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
-        Log.i("tag", "char read $status")
-
-        if (status == BluetoothGatt.GATT_SUCCESS) {
-            // Characteristic read successfully
-            val value = characteristic.value
-            // Process the value as needed
+    override fun onCharacteristicRead(
+        gatt: BluetoothGatt,
+        characteristic: BluetoothGattCharacteristic,
+        value: ByteArray,
+        status: Int
+    ) {
+        super.onCharacteristicRead(gatt, characteristic, value, status)
+        var valueString = "{"
+        for (byte in value) {
+            valueString += "${byte.toString(radix = 16)},"
         }
+        valueString += "}"
+        Log.i("tag", "char read $status: ${valueString}")
     }
 
-    override fun onCharacteristicWrite(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
+    override fun onCharacteristicWrite(
+        gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int
+    ) {
+        super.onCharacteristicWrite(gatt, characteristic, status)
         Log.i("tag", "char write $status")
-
-        if (status == BluetoothGatt.GATT_SUCCESS) {
-            // Characteristic written successfully
-        }
     }
 
-    override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
-        Log.i("tag", "char changed")
+    override fun onCharacteristicChanged(
+        gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, value: ByteArray
+    ) {
+        super.onCharacteristicChanged(gatt, characteristic, value)
+        Log.i("tag", "char changed: ${value}")
+    }
 
-        // Called when a characteristic's value is changed
-        val value = characteristic.value
-        // Process the updated value
+    override fun onDescriptorWrite(
+        gatt: BluetoothGatt?,
+        descriptor: BluetoothGattDescriptor?,
+        status: Int
+    ) {
+        super.onDescriptorWrite(gatt, descriptor, status)
+        Log.i("tag", "desc write $status")
     }
 }
